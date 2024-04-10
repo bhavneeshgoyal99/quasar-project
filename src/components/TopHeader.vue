28764fee7c77a59358825ca764a078c9e4b99093
Bhavneesh Goyal <33285991+bhavneeshgoyal99@users.noreply.github.com>
2024-04-10 19:06:13 +0530

    Initial commit

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav>
+    <a href="#">
+      <img src="" alt="" />
+      <p>Hyperhire</p>
+    </a>
+    <ul>
+      <li>
+        <a href="#">
+          Home
+          <q-icon name="fa-solid fa-chevron-down" />
+        </a>
+      </li>
+      <li><a href="#">About</a></li>
+    </ul>
+    <button>Login</button>
+  </nav>
+</template>
+
+<script setup>
+defineOptions({
+  name: "TopHeader",
+});
+</script>
+<style lang="scss" scoped>
+nav {
+  background-color: transparent;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  width: 80%;
+  margin: auto;
+  a {
+    color: white;
+  }
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    li {
+      float: left;
+      a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        &:hover {
+          background-color: #111;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+defineOptions({
+  name: "MainLayout",
+});
+
+const linksList = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+
+}
+</script>
+
+<style scoped lang="scss">
+.q-layout {
+  background-color: color(srgb 0.2199 0.7008 0.7624) !important;
+}
+</style>
